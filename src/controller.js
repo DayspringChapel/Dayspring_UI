543e6f802c0d6c578768a04421c565fdb888cb7a
olaoluphemy <bidesim8@gmail.com>
2025-01-13 21:57:55 +0100

    Feat: home page hero video background, todos responsiveness, about page partial updates

--- src/controller.js
+++ src/controller.js
@@ -1,1 +1,26 @@
-// console.log("hello world");
+import { ASPECT_RATIO } from "./config.js";
+
+// !!IMPORTANT: PLS DO NOT TAMPER WITH, THIS OPERATION HELPS RESIZE THE HOME PAGE BACKGROUND VIDEO CONTAINER AUTOMATICALLY BASED ON SCREEN-SIZE. ITS NEEDED BECAUSE OF VIDEO'S ASPECT RATIO(IT'LL GET EXTRACTED APPROPRIATELY WHEN JS IMPLEMENTATION STARTS)
+
+const heroEl = document.querySelector(".hero-home");
+const viedoEl = document.querySelector(".video-bg");
+
+function calcVideoHeight() {
+  //   const windowWidth = window.innerWidth;
+  const clientWidth = document.documentElement.clientWidth;
+  const height = `${
+    (ASPECT_RATIO.height * clientWidth) / ASPECT_RATIO.width
+  }px`;
+
+  return height;
+}
+
+(function InitializeVideoHeight() {
+  viedoEl.classList.remove("hide");
+  heroEl.style.height = calcVideoHeight();
+  heroEl.style.backgroundImage = "none";
+})();
+
+window.addEventListener("resize", () => {
+  heroEl.style.height = calcVideoHeight();
+});
