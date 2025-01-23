c80c876e1a59c2e20c3579b3bcca31fefc9fead3
olaoluphemy <bidesim8@gmail.com>
2025-01-23 22:50:04 +0100

    fix: minor ui fixes

--- src/controller.js
+++ src/controller.js
@@ -32,25 +32,35 @@ if (heroEl) {
   });
 }
 
-// Toggle mobile nav visibilty
+// Prevent browser reload when clicked link is same as the currently viewed page
 mobileNav.addEventListener("click", (e) => {
   e.preventDefault();
   const page = e.target.closest(".page");
   const href = e.target.getAttribute("href");
   const windowhref = window.location.pathname.slice(1);
 
-  if (!page) return;
-
-  if ((href === "about.html" && windowhref === "/") || href === windowhref)
+  if (
+    (href === "index.html" && windowhref === "") ||
+    href === windowhref ||
+    !page
+  )
     return;
 
   window.location.pathname = href;
 });
 
+// Toggle mobile nav visibilty
 hamburger.addEventListener("click", () => {
   mobileNav.classList.remove("hide-mobile-nav");
+
+  // prevent document scroll when mobile nav is open
+  setTimeout(() => {
+    document.body.classList.add("body-fixed");
+  }, 500);
 });
 
 mobileNavCloseBtn.addEventListener("click", () => {
+  // reset document scroll when mobile nav is closed
+  document.body.classList.remove("body-fixed");
   mobileNav.classList.add("hide-mobile-nav");
 });
