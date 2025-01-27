a3b1d4e946e343291b6318086e973b0c7ce33188
olaoluphemy <bidesim8@gmail.com>
2025-01-27 23:32:54 +0100

    fix: build error

--- src/galleryView.js
+++ src/galleryView.js
@@ -1,4 +1,4 @@
-import View from "./View.js";
+import View from "./view.js";
 
 class GalleryView extends View {
   constructor() {
--- src/indexView.js
+++ src/indexView.js
@@ -1,4 +1,4 @@
-import View from "./View.js";
+import View from "./view.js";
 import { ASPECT_RATIO } from "./config.js";
 
 class IndexView extends View {
