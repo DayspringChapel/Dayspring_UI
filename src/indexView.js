e7d03d1b3cac42660a2857f7847bf32568281495
olaoluphemy <bidesim8@gmail.com>
2025-06-09 14:31:14 +0100

    feat: theme update, overall layout updates and icon updates

--- src/indexView.js
+++ src/indexView.js
@@ -34,4 +34,50 @@ class IndexView extends View {
   }
 }
 
+// const carousel = document.querySelector(".carousel");
+// const slides = [...document.querySelectorAll(".gallery-image")];
+// let index = 1;
+// const width = 400;
+
+// slides.forEach((el) => console.log(el.dataset.index));
+
+// function moveCarousel() {
+//   carousel.style.transition = "transform 0.5s ease-in-out";
+//   carousel.style.transform = `translateX(${-width * index}px)`;
+// }
+
+// function handleTransitionEnd() {
+//   // console.log(index);
+//   if (slides[index].dataset.index === slides[0].dataset.index) {
+//     carousel.style.transition = "none";
+//     index = 1;
+//     carousel.style.transform = `translateX(${-width * index}px)`;
+//   }
+//   console.log(
+//     slides[index].dataset.index === slides[slides.length - 3].dataset.index,
+//     slides[index].dataset.index,
+//     slides[slides.length - 3].dataset.index,
+//     index
+//   );
+
+//   if (slides[index].dataset.index === slides[slides.length - 3].dataset.index) {
+//     console.log("okay");
+//     carousel.style.transition = "none";
+//     index = slides.length - 2;
+//     console.log(index);
+//     carousel.style.transform = `translateX(${-width * index}px)`;
+//   }
+// }
+
+// carousel.addEventListener("transitionend", handleTransitionEnd);
+
+// function startCarousel() {
+//   setInterval(() => {
+//     index++;
+//     moveCarousel();
+//   }, 1000);
+// }
+
+// startCarousel();
+
 new IndexView();
